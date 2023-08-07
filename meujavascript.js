aa0afcb324882739a57af978123bb3f840b82030
berserk08 <140462871+berserk08@users.noreply.github.com>
2023-08-07 20:45:21 -0300

    Add files via upload

---
+++ meujavascript.js
@@ -0,0 +1,35 @@
+// let primeiroNome = prompt("Digite seu primeiro nome ");
+
+// let sobrenome = prompt("Digite seu sobrenome: ");
+
+// let idade = prompt("Digite sua idade: ");
+
+// const id = 981;
+
+// console.log(`ola, ${primeiroNome} ${sobrenome}, voce tem ${idade} e seu id e ${id}`)
+
+// alert(`ola, ${primeiroNome} ${sobrenome}, voce tem ${idade} e seu id e ${id}`)
+
+
+// const minhaVariavel = 10;
+
+// console.log(typeof minhaVariavel);
+
+
+// console.log("String: 10.7")
+// console.log("number: ", parseFloat("10.7"))
+
+// let novoNumerofloat = 10.978915;
+// console.log(novoNumerofloat.toFixed(2));
+
+
+// let meuNumero = 24;
+// let minhastring = meuNumero + ""
+// console.log(minhastring);
+
+
+// let meuNumero = 69;
+// let minhastring = meuNumero.toString(8);
+// console.log(minhastring);
+
+
